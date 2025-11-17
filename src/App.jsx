daed2f16f696c69df83da8a2d073b02d119f29d0
Musamir5 <musarashid2006@gmail.com>
2025-11-17 01:50:47 +0000

    creating books borrowing page with pie chart

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import LibraryPortal from "./pages/Libraryportal";
 import Digitalregisterstaff from "./pages/Digitalregisterstaff";
 import Librarycollection from "./pages/Librarycollection";
 import Bookdetail from "./pages/Bookdetail";
+import Libraryhistory from "./pages/Libraryhistory";
+
 const App = () => {
   return (
     <div>
@@ -14,14 +16,18 @@ const App = () => {
         <Routes>
           <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
           <Route path="/courses" element={<Courses></Courses>}></Route>
-          <Route path="/timetablestaff"  element={ <Timetablestaff/> }></Route>
-          <Route path="/LibraryPortal"  element={ <LibraryPortal/> }></Route>
-          <Route path="/digitalregisterstaff"  element={ <Digitalregisterstaff/> }></Route>
-          <Route path="/librarycollection" element={<Librarycollection/>}></Route>
-          <Route path="/bookdetail" element={<Bookdetail/>}></Route>
-
-       
-
+          <Route path="/timetablestaff" element={<Timetablestaff />}></Route>
+          <Route path="/LibraryPortal" element={<LibraryPortal />}></Route>
+          <Route
+            path="/digitalregisterstaff"
+            element={<Digitalregisterstaff />}
+          ></Route>
+          <Route
+            path="/librarycollection"
+            element={<Librarycollection />}
+          ></Route>
+          <Route path="/bookdetail" element={<Bookdetail />}></Route>
+          <Route path="/libraryhistory" element={<Libraryhistory />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
