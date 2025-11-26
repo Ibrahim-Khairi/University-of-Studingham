5321b543c19b4e5da75cd30bbc389b7776376893
24001768 <24001768@hope.ac.uk>
2025-11-26 23:24:15 +0000

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import AttendanceDetails from './AttendanceDetails';
+import './App.css';
+import LectureMissed from './LecturesMissed';
+
+function App() {
+  return (
+    <div className="App">
+      <AttendanceDetails />
+      <LectureMissed />
+    </div>
+  );
+}
+
+export default App;
